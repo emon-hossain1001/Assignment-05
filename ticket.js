ebcf022f7f24870625b42c7303d7648c30fbbab4
emon-hossain1001 <mrx00001001@gmail.com>
2024-02-19 22:54:28 +0600

    added js features

--- ticket.js
+++ ticket.js
@@ -1,1 +1,41 @@
+const classes = 'Economy';
+let totalPrice = 0;
+const ticketPrice = 550;
+let seatCount = 0;
+let seatLeft = 40;
+
+
+const busSeats = document.querySelectorAll('.seats');
+
+for(let i = 0; i<busSeats.length; i++){
+    const seat = busSeats[i];
+
+    seat.addEventListener("click", function(){
+
+        // get the title
+        const title = seat.querySelector("span").innerText;
+
+        const titleContainer = document.getElementById('title-container');
+
+        const p = document.createElement("p");
+        p.innerText = title+" "+classes+" "+ticketPrice;
+        titleContainer.appendChild(p);
+
+        // seat count and left
+        seatCount++;
+        document.getElementById('seat-count').innerText = seatCount;
+        seatLeft--;
+        document.getElementById('seat-left').innerText = seatLeft;
+
+        // total price
+        totalPrice += ticketPrice;
+        document.getElementById('total-price').innerText = totalPrice;
+        // seat disabled
+        seat.disabled = true;
+        seat.style.backgroundColor = 'rgb(29,209,0)';
+        
+    });
+};
+
+
 
